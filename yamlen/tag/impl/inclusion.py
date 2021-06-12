e3346ebfdc04b583b60024d5afaaf62622c805ce
ymoch <ymoch.dev@gmail.com>
2021-06-12 17:50:25 +0900

    Simplify.

--- yamlen/tag/impl/inclusion.py
+++ yamlen/tag/impl/inclusion.py
@@ -2,39 +2,25 @@ import glob
 import os
 import re
 
-from yaml import Node, ScalarNode
-from yaml.constructor import BaseConstructor
+from yaml import ScalarNode
 
-from yamlen.loader import Context, Loader, Tag
+from yamlen.loader import Context, Tag
 
 _WILDCARDS_REGEX = re.compile(r"^.*(\*|\?|\[!?.+]).*$")
 
 
 class InclusionTag(Tag):
     def construct_by_context(self, context: Context):
-        return self.construct(
-            context.loader,
-            context.constructor,
-            context.node,
-            context.origin,
-        )
-
-    def construct(
-        self,
-        loader: Loader,
-        constructor: BaseConstructor,
-        node: Node,
-        origin: str = ".",
-    ) -> object:
+        node = context.node
         if not isinstance(node, ScalarNode):
             raise ValueError(f"expected a scalar node, but found {node.tag}")
 
-        base = constructor.construct_scalar(node)
+        base = context.constructor.construct_scalar(node)
         if not base:
             raise ValueError("given no path")
 
-        path = os.path.join(origin, str(base))
+        path = os.path.join(context.origin, str(base))
         if _WILDCARDS_REGEX.match(path):
             paths = glob.iglob(path, recursive=True)
-            return [loader.load_from_path(p) for p in paths]
-        return loader.load_from_path(path)
+            return [context.loader.load_from_path(p) for p in paths]
+        return context.loader.load_from_path(path)
