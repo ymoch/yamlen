03f8107349f03284975c4f4a0d83107548ea2f61
ymoch <ymoch.dev@gmail.com>
2021-06-12 18:45:36 +0900

    Make tests stricter.

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,6 +1,7 @@
 from io import StringIO
+from typing import Optional
 
-from pytest import fixture, raises
+from pytest import raises
 from yaml import ScalarNode
 
 from yamlen import TagContext
@@ -9,28 +10,29 @@ from yamlen.loader import Loader, Tag
 
 
 class BypassTag(Tag):
+    def __init__(self, expected_path: Optional[str]):
+        self._expected_path = expected_path
+
     def construct(self, context: TagContext) -> object:
+        assert context.origin == self._expected_path
+
         node = context.node
         assert isinstance(node, ScalarNode)
         return context.constructor.construct_scalar(node)
 
 
-@fixture
-def loader() -> Loader:
+def test_load_given_invalid_content():
     loader = Loader()
-    loader.add_tag("!bypass", BypassTag())
-    return loader
-
-
-def test_load_given_invalid_content(loader):
     with raises(YamlenError) as error_info:
         loader.load(StringIO("!invalid"))
     assert '", line 1, column 1' in str(error_info.value)
 
 
-def test_load_all(loader):
+def test_load_all():
     stream = StringIO("1\n---\n!bypass 123\n---\n!x\n---\n2")
 
+    loader = Loader()
+    loader.add_tag("!bypass", BypassTag(expected_path=None))
     actual = loader.load_all(stream)
     assert next(actual) == 1
     assert next(actual) == "123"
@@ -40,16 +42,20 @@ def test_load_all(loader):
         next(actual)
 
 
-def test_load_from_path_not_found(mocker, loader):
+def test_load_from_path_not_found(mocker):
     mocker.patch("builtins.open", side_effect=FileNotFoundError("message"))
+
+    loader = Loader()
     with raises(YamlenError):
         loader.load_from_path("/////foo/bar/baz")
 
 
-def test_load_from_path(mocker, loader):
+def test_load_from_path(mocker):
     content = StringIO("!bypass xyz")
     open_mock = mocker.patch("builtins.open", return_value=content)
 
+    loader = Loader()
+    loader.add_tag("!bypass", BypassTag(expected_path="path/to"))
     actual = loader.load_from_path("path/to/scenario.yml")
     assert actual == "xyz"
 
@@ -57,18 +63,21 @@ def test_load_from_path(mocker, loader):
     open_mock.assert_called_once_with("path/to/scenario.yml")
 
 
-def test_load_all_from_path_not_found(mocker, loader):
+def test_load_all_from_path_not_found(mocker):
     mocker.patch("builtins.open", side_effect=FileNotFoundError("message"))
 
+    loader = Loader()
     objs = loader.load_all_from_path("/////foo/bar/baz")
     with raises(YamlenError):
         next(objs)
 
 
-def test_load_all_from_path(mocker, loader):
+def test_load_all_from_path(mocker):
     content = StringIO("1\n---\n!bypass abc\n---\n!x\n---\n2\n")
     open_mock = mocker.patch("builtins.open", return_value=content)
 
+    loader = Loader()
+    loader.add_tag("!bypass", BypassTag(expected_path="path/to"))
     actual = loader.load_all_from_path("path/to/scenario.yml")
     assert next(actual) == 1
     assert next(actual) == "abc"
